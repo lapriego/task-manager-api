b97c896cac690dec0806f420101108716d3e9db6
lapriego <lapriego2005@hotmail.com>
2018-10-30 02:53:43 +0000

    testes de opção

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ require 'api_version_constraint'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  #namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do   
-   namespace :api, defaults: { format: :json }, path: '/' do
-    #namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do   
      namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       resources :tasks
        
